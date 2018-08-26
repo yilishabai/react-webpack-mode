d5ee28b660779c998ed7a67b164e8c13284909c4
Fangluobin <q1217830193@163.com>
2018-08-26 15:24:14 +0800

    react-webpack-mode

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React</h1>
+                <h2>Hello React</h2>
+                <h3>nihao</h3>
+            </div>
+        );
+    }
+}
